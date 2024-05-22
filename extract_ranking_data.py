5e38e4ecf5775256ee077d6d5b0d1f3fba85fecf
saisiddharthnandhakumar <sid@gotdreamsgetbusy.com>
2024-05-22 10:16:16 -0400

    Created cloud function in GCP to trigger a dataflow job to push file on creation from storage bucket to BigQuery

--- extract_ranking_data.py
+++ extract_ranking_data.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+from google.cloud import storage
 
 url = "https://tennisapi1.p.rapidapi.com/api/tennis/rankings/atp/live"
 headers = {
@@ -39,6 +40,16 @@ if response.status_code == 200:
                 writer.writerow(row)
 
         print(f"Data fetched successfully and written to '{csv_filename}'")
+        # Upload the CSV file to GCS
+        bucket_name = 'atp_rankings_tennis'
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        destination_blob_name = f'{csv_filename}'  # The path to store in GCS
+
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(csv_filename)
+
+        print(f"File {csv_filename} uploaded to GCS bucket {bucket_name} as {destination_blob_name}")
     else:
         print("No data available from the API.")
 else:
--- udf.js
+++ udf.js
@@ -1,9 +1,19 @@
 function transform(line) {
-    var values = line.split(',');
-    var obj = new Object();
-    obj.rank = values[0];
-    obj.name = values[1];
-    obj.country = values[2];
-    var jsonString = JSON.stringify(obj);
-    return jsonString;
-   }
+  // Split the input line by commas to extract individual values
+  var values = line.split(",");
+
+  // Create an empty JavaScript object to store the values
+  var obj = new Object();
+
+  // Assign values to the object properties based on their position in the input string
+  obj.player_name = values[0];
+  obj.rank = parseInt(values[1], 10); // Convert to integer using parseInt()
+  obj.country = values[2];
+  obj.points = parseInt(values[3], 10); // Convert to integer using parseInt()
+
+  // Convert the JavaScript object to a JSON string
+  var jsonString = JSON.stringify(obj);
+
+  // Return the JSON string
+  return jsonString;
+}
